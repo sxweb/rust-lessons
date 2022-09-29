0b94d34136fd83f8e1ddbd0e4c665f5c737032cd
Yuriy Vozhagov <phpreddd@gmail.com>
2022-09-29 10:42:35 +0300

    for, loop, while lesson

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,23 @@
 
 fn main(){
+    let mut counter = 0;
     let mut lang = ["C#", "Rust", "Python"];
     lang[0] = "Java Script";
 
     for i in lang.iter(){
         println!("Lang: {}", i);
+    }
+
+    loop{        
+        counter += 1;
+        if counter >= 5{
+            break;
+        }
+        println!("I love Rust");
+    }
+    let mut some = true;
+    while some {
+        println!("Value: {}", some);
+        some = !some;
     }
 }
