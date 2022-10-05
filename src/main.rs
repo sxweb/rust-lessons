4967a0e4993b86bcfbf547af5cc6816974ea3234
Yuriy Vozhagov <phpreddd@gmail.com>
2022-10-05 17:03:32 +0300

    ownership lesson

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,35 @@
 fn main(){
-    let message = "i love Rust";
-    let func = ||{
-        println!("message: {}", message);
-    };
+    let x = "hello rust".to_string();
 
-    func();
+    let data = x;
 
-    let mut num = 0;
-    let mut add_one = ||{
-        num += 1;
-    };
+    //we cant use this var because the owner changed
+    //println!("It's incorrect: {}", x);
+    println!("we can use this var: {}", data);
 
-    add_one();
-    add_one();
-    add_one();
+    //and we can use clone func to copy the value
+    let new_data = data.clone();
+    println!("some data: {}", new_data);
+
+    //if we use the value in the function we delegate the ownership to it
+    //and we cant use this var in future
+    let x = "some string".to_string();
+    print( x);
+    //and we cant use this:
+    //println!("cant use: {}", x);
+
+    //but we can also make a ref for variable
+    let x = "value for x".to_string();
+    let ref_x = &x;
+    print_ref(ref_x);
+    //and we steel can use x variable
+    print(x);
+}
+
+fn print(value: String){
+    println!("Now the func own this value: {}", value);
+}
+
+fn print_ref(value: &String){
+    println!("Now the func use ref: {}", value);
 }
