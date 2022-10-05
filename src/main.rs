0de7807c08e79c929e00addc079c9120c83a5493
Yuriy Vozhagov <phpreddd@gmail.com>
2022-10-05 17:47:02 +0300

    methods and constructors for structs lesson

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,36 @@
+struct Developer{
+    name: String,
+    age: i32,
+    lang: String
+}
+
+impl Developer{
+    fn get_age(&self)->i32{
+        self.age
+    }
+
+    fn change_lang(&mut self, new_lang: String){
+        self.lang = new_lang;
+    }
+
+    fn new(age: i32, name: String, lang: String) -> Developer{
+        Developer{age, name, lang}
+    }
+}
+
+
+
 fn main(){
-    let x = "hello rust".to_string();
+    let mut dev = Developer{
+        name: "Yuriy".to_string(),
+        age: 37,
+        lang: "Java Script".to_string()
+    };
 
-    let data = x;
+    let age = dev.get_age();
+    println!("Dev age: {}", age);
+    dev.change_lang("Rust".to_string());
 
-    //we cant use this var because the owner changed
-    //println!("It's incorrect: {}", x);
-    println!("we can use this var: {}", data);
-
-    //and we can use clone func to copy the value
-    let new_data = data.clone();
-    println!("some data: {}", new_data);
-
-    //if we use the value in the function we delegate the ownership to it
-    //and we cant use this var in future
-    let x = "some string".to_string();
-    print( x);
-    //and we cant use this:
-    //println!("cant use: {}", x);
-
-    //but we can also make a ref for variable
-    let x = "value for x".to_string();
-    let ref_x = &x;
-    print_ref(ref_x);
-    //and we steel can use x variable
-    print(x);
+    let new_dev = Developer::new(33, "John".to_string(), "C#".to_string());
 }
 
-fn print(value: String){
-    println!("Now the func own this value: {}", value);
-}
-
-fn print_ref(value: &String){
-    println!("Now the func use ref: {}", value);
-}
