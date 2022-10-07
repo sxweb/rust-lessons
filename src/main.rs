96a142a05f6d001e791ef379cbcdc9770e5abe7f
Yuriy Vozhagov <phpreddd@gmail.com>
2022-10-07 12:53:00 +0300

    methods practice

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,38 @@
-struct Developer{
-    name: String,
-    age: i32,
-    lang: String
+struct Car{
+    model: String,
+    year: i32,
+    color: String,
 }
 
-impl Developer{
-    fn get_age(&self)->i32{
-        self.age
+impl Car{
+    fn get_year(&self)->&i32{
+        &self.year
     }
 
-    fn change_lang(&mut self, new_lang: String){
-        self.lang = new_lang;
+    fn change_color(&mut self, color:String){
+        self.color = color;
     }
 
-    fn new(age: i32, name: String, lang: String) -> Developer{
-        Developer{age, name, lang}
+    fn new(model: String, year: i32, color: String) -> Car{
+        Car{
+            model,
+            year,
+            color
+        }
     }
 }
-
-
 
 fn main(){
-    let mut dev = Developer{
-        name: "Yuriy".to_string(),
-        age: 37,
-        lang: "Java Script".to_string()
+    let mut car = Car{
+        model: "Honda".to_string(),
+        year: 2015,
+        color: "Red".to_string()
     };
 
-    let age = dev.get_age();
-    println!("Dev age: {}", age);
-    dev.change_lang("Rust".to_string());
+    println!("Year: {}", car.get_year().to_string());
+    car.change_color("Black".to_string());
 
-    let new_dev = Developer::new(33, "John".to_string(), "C#".to_string());
+    let new_car = Car::new("BMW".to_string(), 2021, "Yellow".to_string());
+    let year = new_car.get_year();
+    println!("Year of new car: {}", year);
 }
-
