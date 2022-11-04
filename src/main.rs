31acc91085c05fb1691098715362ca22b0340981
Yuriy Vozhagov <phpreddd@gmail.com>
2022-11-04 12:46:10 +0300

    match lesson

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,15 @@
-struct Car{
-    model: String,
-    color: String,
-    year: i32
-}
-
-struct Person{
-    name: String,
-    age: i32,
-    car: Car
-}
-
 fn main(){
-    let name = "Yuriy";
-    println!("Hello, {}", name);
-    let mut lada = Car{
-        model: "Lada".to_string(),
-        color: "Yellow".to_string(),
-        year: 2008
-    };
-    let mut bmw = Car{
-        model: "BMW".to_string(),
-        color: "Black".to_string(),
-        year: 2010
-    };
+    let num = 10;
 
-    let mut yuriy = Person{
-        name: "Yuriy".to_string(),
-        age: 37,
-        car: lada
-    };
-
-    buy_car(yuriy, bmw);
+    match num {
+        15 => println!("the num is 15"),
+        10 => {
+            println!("the num is 10");
+            println!("the num matched");
+        },
+        5..=50 =>{
+            println!("The num is between 5 and 10");
+        },
+        _ => println!("no any matches")
+    }
 }
-
-fn buy_car(person : Person, car : Car){
-    person.car = car;
-    println!("Congr, you got a {}", car.model.to_string());
-}
-
